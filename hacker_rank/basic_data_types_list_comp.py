cb824eb961276c5bd0d1a221f23dc1adb2e49dc2
Dahmear Johnson <116639830+dahjohnson@users.noreply.github.com>
2022-12-25 13:29:50 -0500

    Fixed syntax issues

--- hacker_rank/basic_data_types_list_comp.py
+++ hacker_rank/basic_data_types_list_comp.py
@@ -3,6 +3,6 @@ y = int(input('Enter y coordinate: '))
 z = int(input('Enter z coordinate: '))
 n = int(input('Enter n interger: '))
 
-coord = [[i, j, k] (for i in range(x+1)) for j in range(y+1) for k in range(z+1) if (i + j + k != n)]
+coord = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k != n)]
 
 print(coord)
